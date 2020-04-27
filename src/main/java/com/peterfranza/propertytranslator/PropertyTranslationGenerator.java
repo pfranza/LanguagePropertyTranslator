a531f9cce3b8cad14c2aa3a5fd98a0eb3581dfae
peter <pfranza@gmail.com>
2020-04-27 10:51:01 -0400

    removing meta processor, since everybody hates it. Added an optional classpath scanner where the property generator will consider files from inside dependency jars that are added to the classpath

--- src/main/java/com/peterfranza/propertytranslator/PropertyTranslationGenerator.java
+++ src/main/java/com/peterfranza/propertytranslator/PropertyTranslationGenerator.java
@@ -5,16 +5,21 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -23,7 +28,10 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.shared.model.fileset.FileSet;
 import org.apache.maven.shared.model.fileset.util.FileSetManager;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
 
+import com.google.common.base.Strings;
 import com.peterfranza.propertytranslator.translators.DictionaryTranslator.CleanProperties;
 
 @Mojo(name = "generate-languages", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
@@ -38,100 +46,102 @@ public class PropertyTranslationGenerator extends AbstractMojo {
 	@Parameter(required = true)
 	String sourceLanguage;
 
-	@Parameter(defaultValue = "PropertyMeta", required = false)
-	String metaSuffix = "PropertyMeta";
+	@Parameter(required = false, defaultValue=".*\\_en.properties")
+	String dependencyMatcher;
 	
-	@Parameter(defaultValue = "Package", required = false)
-	String packageOutputName = "Package";
-	
-	@Parameter(required = false)
-	String rootPropertyClass;
-	
-	@Parameter(required=false, defaultValue="false")
-	public boolean generateMeta = false;
-
 	private FileSetManager fileSetManager = new FileSetManager();
 
 	@Parameter(defaultValue = "${project.build.directory}/generated-sources/properties", required = true)
 	public File targetFolder;
-	
+
 	@Parameter(defaultValue = "${project.build.sourceDirectory}", required = true)
 	public File sourceFolder;
 
 	public void execute() throws MojoExecutionException, MojoFailureException {
 
+		//This is a bag of work items
+		List<TranslatableWorkSourceInput> translatableItems = new ArrayList<TranslatableWorkSourceInput>();
+		
 		try {
 			File root = new File(fileset.getDirectory());
 
-			for (TranslatorConfig t : translators) {
+			
+			
+			Reflections reflections = new Reflections(new ResourcesScanner());
+			Set<String> properties = reflections.getResources(Pattern.compile(dependencyMatcher));
+			
+			//Process classpath items
+			properties.stream().forEach(str -> {
+
+				try {
+					Properties packageProperties = new Properties();
+					packageProperties.load(PropertyTranslationGenerator.class.getResourceAsStream("/" + str));
+					
+					
+					TranslatableWorkSourceInput input = new TranslatableWorkSourceInput(TranslatableWorkSourceInputType.DEPENDENCY, 
+							Utils.getPackageNameFor(str),
+							str, packageProperties, null);
+					
+					translatableItems.add(input);
+					
+				} catch(Exception e) {
+					throw new RuntimeException(e);
+				}
+			});
+			
+			List<String> includedFiles = Arrays.asList(fileSetManager.getIncludedFiles(fileset));
+			includedFiles.parallelStream().forEach(throwingConsumerWrapper(f -> {
+				
+				File inputFile = new File(root, f);
+				try(FileInputStream fis = new FileInputStream(inputFile)) {
+					Properties sourceProperties = new Properties();
+					sourceProperties.load(fis);
+
+					TranslatableWorkSourceInput input = new TranslatableWorkSourceInput(TranslatableWorkSourceInputType.JAVA_PACKAGE_SOURCE, 
+							Utils.getPackageNameFor(root, f, getLog()),
+							f, sourceProperties, inputFile);
+					
+					translatableItems.add(input);
+				}
+			}));
+			
+			
+			List<TranslatableWorkItem> workItems = merge(translatableItems);
+			
+			Arrays.asList(translators).parallelStream().forEach(throwingConsumerWrapper(t -> {
 				t.type.getTranslator().reconfigure(t, sourceLanguage);
 				t.type.getTranslator().open();
 
-				for (String f : fileSetManager.getIncludedFiles(fileset)) {
-					getLog().info("Processing: " + f + " for " + t.targetLanguage);
-					generateLanguagePropertyFiles(root, getOutputRoot(root), t, f);
-				}
+				workItems.parallelStream().forEach(throwingConsumerWrapper(workItem -> {
+					getLog().info("Processing: " + workItem.getName() + " for " + t.targetLanguage);
+					generateLanguagePropertyFiles(root, getOutputRoot(root), t, workItem);
+				}));
+				
 				t.type.getTranslator().printStats(getLog());
 				t.type.getTranslator().close();
-			}
-
-			if(generateMeta) {
-				Set<PropertyFileMetaContainer> metaContainers = new HashSet<>();
-				ExecutorService pool = Executors.newFixedThreadPool(4);
-				for (String f : fileSetManager.getIncludedFiles(fileset)) {
-					pool.submit(new Runnable() {
-						@Override
-						public void run() {
-							try {
-							if(isFileInSourceDirectory(new File(root, f))) {
-								metaContainers.add(new PropertyFileMetaContainer(root, f, metaSuffix, packageOutputName, rootPropertyClass, sourceLanguage));
-							}
-							} catch(Exception e) {
-								getLog().error(e);
-							}
-						}
-					});
-				}
-				
-				pool.shutdown();
-				pool.awaitTermination(1, TimeUnit.HOURS);
-
-				metaContainers.stream().forEach(r -> {
-					r.attachParent(metaContainers);
-				});
-
-				getOutputRoot(root).mkdirs();
-				metaContainers.stream().forEach(r -> {
-					getLog().info("Generating Property Meta For " + r.getName());
-					try {
-						r.generate(getOutputRoot(root));
-					} catch (Exception e) {
-						throw new RuntimeException(e);
-					}
-				});
-			}
-
+			}));
+		
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new MojoFailureException("Error processing translations", e);
 		}
+		
+		
 	}
 
 	private void generateLanguagePropertyFiles(File inputRoot, File outputRoot, TranslatorConfig config,
-			String filename) throws FileNotFoundException, IOException, Exception {
+			TranslatableWorkItem workItem) throws FileNotFoundException, IOException, Exception {
 		outputRoot.mkdirs();
-		
-		File inputFile = new File(inputRoot, filename);
-		FileInputStream fis = new FileInputStream(inputFile);
-
-		Properties source = new Properties();
-		source.load(fis);
-
-		Properties target = new CleanProperties();
-
-		File outFile = getOutputFile(inputFile, inputRoot, outputRoot, config, filename);
+		File outFile = workItem.getOutputFile(outputRoot, config);
 		outFile.getParentFile().mkdirs();
-		FileOutputStream fout = new FileOutputStream(outFile);
+
+		try(FileOutputStream fout = new FileOutputStream(outFile)) {
+			generateLanguagePropertyFiles(config, workItem.getProperties(), fout);	
+		}		
+	}
+	
+	private void generateLanguagePropertyFiles(TranslatorConfig config, Properties source, OutputStream output) throws Exception {
+		Properties target = new CleanProperties();
 		for (Entry<Object, Object> entry : source.entrySet()) {
 			String v = config.type.getTranslator().translate(entry.getValue().toString());
 			if (v != null) {
@@ -141,21 +151,9 @@ public class PropertyTranslationGenerator extends AbstractMojo {
 			}
 		}
 
-		target.store(fout, null);
-		fout.close();
-		fis.close();
+		target.store(output, null);
 	}
 
-	private File getOutputFile(File inputFile, File inputRoot, File outputRoot, TranslatorConfig config, String filename) {
-		if(isFileInSourceDirectory(inputFile)) {			
-			Path first = Paths.get(sourceFolder.toURI()); 
-			Path second = Paths.get(inputFile.getParentFile().toURI());
-			return new File(outputRoot, first.relativize(second).toString() + File.separator + replaceLast(inputFile.getName(), sourceLanguage, config.targetLanguage));
-		} else {
-			return new File(inputRoot, replaceLast(filename, sourceLanguage, config.targetLanguage));
-		}
-	}
-	
 	private boolean isFileInSourceDirectory(File inputFile) {
 		return Paths.get(inputFile.toURI()).startsWith(Paths.get(sourceFolder.toURI()));
 	}
@@ -175,5 +173,157 @@ public class PropertyTranslationGenerator extends AbstractMojo {
 
 		return string.substring(0, index) + replacement + string.substring(index + substring.length());
 	}
+
+	public static <T> Consumer<T> throwingConsumerWrapper(ThrowingConsumer<T, Exception> throwingConsumer) {
+
+		return i -> {
+			try {
+				throwingConsumer.accept(i);
+			} catch (Exception ex) {
+				throw new RuntimeException(ex);
+			}
+		};
+	}
+
+	@FunctionalInterface
+	public interface ThrowingConsumer<T, E extends Exception> {
+		void accept(T t) throws E;
+	}
+	
+	private enum TranslatableWorkSourceInputType {
+		DEPENDENCY, JAVA_PACKAGE_SOURCE, RAW_FILE
+	}
+	
+	private List<TranslatableWorkItem> merge(List<TranslatableWorkSourceInput> input) {
+		ArrayList<TranslatableWorkItem> work = new ArrayList<PropertyTranslationGenerator.TranslatableWorkItem>();
+		
+		Consumer<TranslatableWorkSourceInput> itemProcessor = (item) -> {
+			Optional<TranslatableWorkItem> existingWorkItem = 
+					work.parallelStream().filter(w -> w.isFor(item)).findFirst();
+			
+			if(existingWorkItem.isPresent()) {
+				existingWorkItem.get().add(item);
+			} else {
+				work.add(new TranslatableWorkItem().add(item));
+			}
+		};
+		
+		input.stream().filter(s -> s.type == TranslatableWorkSourceInputType.DEPENDENCY).forEach(itemProcessor);		
+		input.stream().filter(s -> s.type != TranslatableWorkSourceInputType.DEPENDENCY).forEach(itemProcessor);
+		
+		return work;
+	}
+	
+	private static class TranslatableWorkSourceInput {
+		private String sourcePath;
+		private String packagePath;
+		
+		private Properties input;
+		private File inputFile;
+		private TranslatableWorkSourceInputType type;
+		
+		public TranslatableWorkSourceInput(TranslatableWorkSourceInputType type, String packagePath, String sourcePath, Properties input, File inputFile) {
+			super();
+			this.type = type;
+			this.sourcePath = sourcePath;
+			this.input = input;
+			this.packagePath = packagePath;
+			this.inputFile = inputFile;
+		}
+		
+		public String getUnitName() {
+			if(sourcePath.lastIndexOf("/") != -1) {
+				return sourcePath.substring(sourcePath.lastIndexOf("/"));
+			} 
+			
+			return sourcePath;
+		}
+
+		@Override
+		public String toString() {
+			return "TranslatableWorkSourceInput [type=" + type + ", packagePath=" + packagePath + ", sourcePath="
+					+ sourcePath 
+					//+ ", input=" + input
+					+ "]";
+		}
+		
+	}
+	
+	private class TranslatableWorkItem {
+		
+		private List<TranslatableWorkSourceInput> sources = new ArrayList<TranslatableWorkSourceInput>();	
+		
+		public TranslatableWorkItem add(TranslatableWorkSourceInput item) {
+			this.sources.add(item);
+			return this;
+		}
+		
+		public String getName() {
+			
+			Optional<TranslatableWorkSourceInput> src = sources.stream().filter(s -> s.type != TranslatableWorkSourceInputType.DEPENDENCY).findFirst();
+			if(src.isPresent()) {
+				return src.get().sourcePath;
+			}
+			
+			src = sources.stream().filter(s -> s.type == TranslatableWorkSourceInputType.DEPENDENCY).findFirst();
+			if(src.isPresent()) {
+				return src.get().sourcePath;
+			}
+			
+			return "Unknown";
+		}
+
+		public Properties getProperties() {
+			Properties target = new CleanProperties();
+			
+			sources.stream().filter(s -> s.type == TranslatableWorkSourceInputType.DEPENDENCY).forEach(s -> {
+				target.putAll(s.input);
+			});
+			
+			sources.stream().filter(s -> s.type != TranslatableWorkSourceInputType.DEPENDENCY).forEach(s -> {
+				target.putAll(s.input);
+			});
+			
+			return target;
+		}
+		
+		public File getOutputFile(File outputRoot, TranslatorConfig config) {
+			
+			for(TranslatableWorkSourceInput source: sources) {
+				if(source.inputFile != null) {
+					if (isFileInSourceDirectory(source.inputFile)) {
+						Path first = Paths.get(sourceFolder.toURI());
+						Path second = Paths.get(source.inputFile.getParentFile().toURI());
+						return new File(outputRoot, first.relativize(second).toString() + File.separator
+								+ replaceLast(source.inputFile.getName(), sourceLanguage, config.targetLanguage));
+					} else {
+						return new File(new File(fileset.getDirectory()), replaceLast(source.sourcePath, sourceLanguage, config.targetLanguage));
+					}
+				}
+			}
+			
+			for(TranslatableWorkSourceInput source: sources) {
+				return new File(targetFolder, replaceLast(source.sourcePath, sourceLanguage, config.targetLanguage));
+			}
+			
+			throw new RuntimeException("Unable to determine output file");
+		}
+		
+		public boolean isFor(TranslatableWorkSourceInput item) {
+
+			if(!Strings.isNullOrEmpty(item.packagePath) && sources.size() > 0) {
+				for(TranslatableWorkSourceInput source: sources) {
+					if(source.packagePath.equalsIgnoreCase(item.packagePath)) {
+						//They are in same package .. but are they the same source
+						return source.getUnitName().equalsIgnoreCase(item.getUnitName());
+					}
+				}
+			}
+			
+			return false;
+		}
+		
+	}
+	
 
 }
---
+++ src/main/java/com/peterfranza/propertytranslator/Utils.java
@@ -0,0 +1,41 @@
+package com.peterfranza.propertytranslator;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.apache.maven.plugin.logging.Log;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+
+public class Utils {
+
+	public static String getPackageNameFor(String resource) {
+		return resource.substring(0, resource.lastIndexOf("/")).replace("/", ".");
+	}
+	
+	public static String getPackageNameFor(File inputRoot, String filename, Log logger) throws FileNotFoundException, IOException {
+		File parentFolder = new File(inputRoot, filename).getParentFile();
+		Optional<File> file = Arrays.asList(parentFolder.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(".java");
+			}
+		})).stream().findFirst();
+	
+		if (file.isPresent()) {
+			CompilationUnit unit = JavaParser.parse(file.get());
+			if (unit.getPackageDeclaration().isPresent()) {
+				return unit.getPackageDeclaration().get().getNameAsString();
+			}
+		}
+		
+		logger.warn("Package not found for " + filename + ", if this is a java property create a package-info.java sibling in same directory");
+		return filename.replace(".properties", "").replace("/", ".");
+	}
+	
+}
